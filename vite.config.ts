5126a25fab98a9a18e65a2e63975c404205b37d3
vardhan241 <vardhan241241@gmail.com>
2026-06-08 15:39:39 +0530

    fix tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {}
-  }
+  plugins: [react(), tailwindcss()],
 })

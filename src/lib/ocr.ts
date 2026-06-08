49472dcf26fc7efcf73cedd9c7084ed57793641f
vardhan241 <vardhan241241@gmail.com>
2026-06-09 01:13:55 +0530

    fix ocr url to production

--- src/lib/ocr.ts
+++ src/lib/ocr.ts
@@ -1,29 +1,24 @@
-const API_URL = "http://127.0.0.1:8001";
+const API_URL = "https://agon-backend-wl3r.onrender.com"\;
 
 export async function performOCR(file: File) {
   try {
     const formData = new FormData();
     formData.append("file", file);
-
     const response = await fetch(`${API_URL}/scan_plate`, {
       method: "POST",
       body: formData,
     });
-
     const data = await response.json();
-
     if (data.success && data.plate_number) {
       return {
         plateNumber: data.plate_number,
       };
     }
-
     return {
       plateNumber: null,
     };
   } catch (error) {
     console.error(error);
-
     return {
       plateNumber: null,
     };
